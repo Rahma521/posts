cde01f09194183ea77b27b7264b2ac5978b571e0
Rahma AbdEl Aziz <abdelazizrahma82@gmail.com>
2020-08-16 13:55:14 +0200

    second commit

--- djangoProject4/settings.py
+++ djangoProject4/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'topic',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,4 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
+LOGIN_REDIRECT_URL = 'home'
